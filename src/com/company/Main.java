a8e4721d59d9ad2cbb504a98c35ec0f8fee324d2
Olegka <nikitov.00@mail.ru>
2020-03-13 17:41:08 +0300

    program for solving quadratic equations

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,32 @@
 package com.company;
 
+import static java.lang.StrictMath.sqrt;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        int a = 15;
-        System.out.println(a);
+        int a, b, c, D;
+        double x1, x2;
+        a = 1;
+        b = 2;
+        c = 1;
+        D = b * b - (4 * a * c);
+        if (D > 0) {
+            x1 = (-b + sqrt(D)) / (2 * a);
+            x2 = (-b - sqrt(D)) / (2 * a);
+            System.out.println("Корни уравнения равны:");
+            System.out.println("x1=" + x1);
+            System.out.println("x2=" + x2);
+        }
+        if (D == 0) {
+            x1 = (-b + sqrt(D)) / (2 * a);
+            System.out.println("Единственный корень уравнения равен:");
+            System.out.println("x=" + x1);
+        }
+        if (D < 0) {
+            System.out.println("Корней уравнения нет");
+        }
     }
+
 }
